59dadc6492cf417d04dd1a96ddec62909440ca4f
hesronkija <hesronkkz@icloud.com>
2024-05-31 23:18:09 -0600

    modificatons

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,25 +15,29 @@ const Projects = () => {
       {PROJECTS.map((project, index) => (
         <motion.div
           key={index}
-          className="mb-4 flex flex-col gap-4 md:flex-row"
+          className="mb-8 flex flex-col gap-4 md:flex-row"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: index * 0.2 }}
         >
-          <div className="w-full flex flex-col md:w-1/2">
-            <h2 className="text-lg font-bold text-center md:text-start">{project.heading}</h2>
-            <p className="md:pr-16 text-sm text-orange-400/70 text-center md:text-start">{project.intro}</p>
+          <div className="flex w-full flex-col md:w-1/3">
+            <h2 className="text-center text-lg font-bold md:text-start">
+              {project.heading}
+            </h2>
+            <p className="text-center text-sm text-orange-400/70 md:pr-16 md:text-start">
+              {project.intro}
+            </p>
             <ul>
               {project.titles.map((title, idx) => (
                 <motion.li
                   key={idx}
-                  className="mb-2 pl-4 text-center md:text-start text-sm text-neutral-100/70"
+                  className="mb-2 pl-4 text-center text-sm text-neutral-100/70 md:text-start"
                   initial={{ opacity: 0 }}
                   whileInView={{ opacity: 1 }}
                   transition={{ duration: 0.5, delay: idx * 0.1 }}
                 >
                   <a
-                    className="underline md:no-underline hover:underline hover:cursor-pointer"
+                    className="underline hover:cursor-pointer hover:underline md:no-underline"
                     href={project.links}
                     target="_blank"
                     rel="noopener noreferrer"
@@ -45,9 +49,9 @@ const Projects = () => {
             </ul>
           </div>
 
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <div className="flex w-full justify-center md:w-2/3 md:justify-center">
             <motion.img
-              className="w-5/6 rounded shadow-lg shadow-orange-500/15"
+              className="w-3/6 rounded shadow-lg shadow-orange-500/15"
               src={project.photo}
               alt={project.heading}
               initial={{ opacity: 0 }}
